d741ba6ea256f117525a2ce0d781ad1319531d82
nebilesamelsuleymna <nebilesmaelprog@gmail.com>
2025-02-26 08:48:23 -0900

    navigation

---
+++ src/components/HeaderItem.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+function HeaderItem({ Icon, name }) {
+	return (
+		<div className='text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8'>
+			<Icon />
+			<h2>{name}</h2>
+		</div>
+	)
+}
+export default HeaderItem
